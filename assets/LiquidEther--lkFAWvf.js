bacd52e5e09459e192133f767ce8201c87201ec6
AlexanderG-CA <181277804+AlexanderG-CA@users.noreply.github.com>
2026-07-16 17:04:47 +0200

    Updates

--- assets/LiquidEther--lkFAWvf.js
+++ assets/LiquidEther--lkFAWvf.js
@@ -1,4 +1,4 @@
-import{r as on,j as _l}from"./index-BUoY6pa-.js";/**
+import{r as on,j as _l}from"./index-BMaNIVhX.js";/**
  * @license
  * Copyright 2010-2026 Three.js Authors
  * SPDX-License-Identifier: MIT
