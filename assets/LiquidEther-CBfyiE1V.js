405405eaf00d1cb198041761373bea3b3436f5f6
AlexanderG-CA <181277804+AlexanderG-CA@users.noreply.github.com>
2026-08-03 01:40:25 +0200

    Updates

--- assets/LiquidEther-CBfyiE1V.js
+++ assets/LiquidEther-CBfyiE1V.js
@@ -1,4 +1,4 @@
-import{r as on,j as _l}from"./index-B8wvE0Au.js";/**
+import{r as on,j as _l}from"./index-DdYeZ1u6.js";/**
  * @license
  * Copyright 2010-2026 Three.js Authors
  * SPDX-License-Identifier: MIT
